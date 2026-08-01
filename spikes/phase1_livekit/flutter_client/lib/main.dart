6a21f5dc40b07cfba640269e66ff3a02b7730e4e
Dharmendra Gupta <mani.gupta103@gmail.com>
2026-08-02 05:17:58 +0530

    feat(phase1): add Flutter spike entry point

---
+++ spikes/phase1_livekit/flutter_client/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:ai_teacher_phase1_livekit/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const Phase1SpikeApp());
+}
